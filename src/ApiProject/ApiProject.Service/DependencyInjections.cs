fed45c6bb3f51b7e2a36006773883f0fc3a89fbd
binxiong <binxiong.tea@redone.com.my>
2024-05-07 23:15:50 +0800

    Add Application Service Dependency Injections extension

---
+++ src/ApiProject/ApiProject.Service/DependencyInjections.cs
@@ -0,0 +1,23 @@
+﻿using ApiProject.Domain.Orders;
+using ApiProject.Service.Orders;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApiProject.Service
+{
+	public static class DependencyInjections
+	{
+		public static IServiceCollection InstallApplicationServices(this IServiceCollection services)
+		{
+			services.AddTransient<OrderService>();
+
+			services.AddTransient<IOrderHandler, OrderHandler>();
+
+			return services;
+		}
+	}
+}
