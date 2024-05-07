c271339a3aff861558db17a215f70cc1dedfe5a7
binxiong <binxiong.tea@redone.com.my>
2024-05-07 23:09:21 +0800

    Add AccountingRepository class

---
+++ src/ApiProject/ApiProject.Domain/Accountings/IAccountingRepository.cs
@@ -0,0 +1,14 @@
+﻿using ApiProject.Domain.Products;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApiProject.Domain.Accounts
+{
+	public interface IAccountingRepository
+	{
+		Money GetTaxAmount(Currency currency);
+	}
+}
