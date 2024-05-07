89d1725b5a4e76804025013d9ff35f1cc51e9a9a
binxiong <binxiong.tea@redone.com.my>
2024-05-07 18:39:26 +0800

    Add validation to Money class, overloading the plus operator

--- src/ApiProject/ApiProject.Domain/Products/Money.cs
+++ src/ApiProject/ApiProject.Domain/Products/Money.cs
@@ -13,5 +13,16 @@ namespace ApiProject.Domain.Products
 		public static Money Zero(Currency currency) => new(0, currency);
 
 		public bool IsZero() => this == Zero();
+
+		public static Money operator +(Money first, Money second)
+		{
+			if (first.Currency != second.Currency)
+			{
+				throw new InvalidOperationException("Currencies have to be equal");
+			}
+
+			return new Money(first.Amount + second.Amount, first.Currency);
+		}
+
 	}
 }
