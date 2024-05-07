68b883c84a0cad4087b7cf8c97657ae013ce00ea
binxiong <binxiong.tea@redone.com.my>
2024-05-07 22:04:11 +0800

    Add OrderErrors class

---
+++ src/ApiProject/ApiProject.Domain/Orders/OrderErrors.cs
@@ -0,0 +1,14 @@
+﻿using ApiProject.Domain.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApiProject.Domain.Orders
+{
+	public static class OrderErrors
+	{
+		public static Error NotFound = new("Order.NotFound", "The order with the specified id was not found");
+	}
+}
