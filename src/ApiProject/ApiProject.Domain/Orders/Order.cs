487b329e2bc68d6488279372bcef3b6cbb806d07
binxiong <binxiong.tea@redone.com.my>
2024-05-07 17:55:24 +0800

    Add OrderStatus class & Add Order class properties

--- src/ApiProject/ApiProject.Domain/Orders/Order.cs
+++ src/ApiProject/ApiProject.Domain/Orders/Order.cs
@@ -1,6 +1,8 @@
 ﻿using ApiProject.Domain.Abstractions;
+using ApiProject.Domain.Products;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,6 +11,42 @@ namespace ApiProject.Domain.Orders
 {
 	public class Order: Entity
 	{
+		public List<Product> products { get;private set; }
 
+		public Money TotalPrice
+		{
+			get
+			{
+				decimal totalPrice = products.Sum((a) => a.Price.Amount);
+				return (products.Count == 0) ? 
+					new Money(0, Currency.None):
+					new Money(totalPrice, products.First().Price.Currency);
+			}
+		}
+
+		public Money TotalPriceAfterTax
+		{
+			get
+			{
+				decimal totalPrice = products.Sum((a) => a.AmountAfterTax.Amount);
+				return (products.Count == 0) ?
+					new Money(0, Currency.None) :
+					new Money(totalPrice, products.First().Price.Currency);
+			}
+		}
+
+		public OrderStatus Status { get; private set; }
+
+		public DateTime CreatedOnUtc { get; private set; }
+
+		public DateTime ConfirmedOnUtc { get; private set; }
+
+		public DateTime AcceptedOnUtc { get; private set; }
+
+		public DateTime DeliveredOnUtc { get; private set; }
+
+		public DateTime CompletedOnUtc { get; private set; }
+
+		public DateTime CancelledOnUtc { get; private set; }
 	}
 }
---
+++ src/ApiProject/ApiProject.Domain/Orders/OrderStatus.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApiProject.Domain.Orders
+{
+	public enum OrderStatus
+	{ 
+		Confirmed = 1,
+		Accepted = 2,
+		Delivered = 3,
+		Cancelled = 4,
+		Completed = 5
+	}
+}
