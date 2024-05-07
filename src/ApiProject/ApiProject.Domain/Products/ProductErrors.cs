30306cc57125b6c7c54089e2ad6e426978467f9d
binxiong <binxiong.tea@redone.com.my>
2024-05-07 17:33:21 +0800

    Add ProductErrors

---
+++ src/ApiProject/ApiProject.Domain/Products/ProductErrors.cs
@@ -0,0 +1,17 @@
+﻿using ApiProject.Domain.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApiProject.Domain.Products
+{
+	public static class ProductErrors
+	{
+		public static Error NotFound = new(
+			"Product.NotFound",
+			"The product with the specified id was not found"
+			);
+	}
+}
