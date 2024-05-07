386b81bcbe6146f5879338ec956bd8b23962ee19
binxiong <binxiong.tea@redone.com.my>
2024-05-07 17:33:44 +0800

    Add ProductRepository interface

---
+++ src/ApiProject/ApiProject.Domain/Products/IProductRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApiProject.Domain.Products
+{
+	public interface IProductRepository
+	{
+		Task<Product?> GetByIdAsync(long id, CancellationToken cancellationToken = default);
+	}
+}
