70b455b17d72f3f2551422c3f574d30e2ab2e3aa
binxiong <binxiong.tea@redone.com.my>
2024-05-07 23:10:06 +0800

    Update OrderRepository interface

---
+++ src/ApiProject/ApiProject.Domain/Orders/IOrderRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApiProject.Domain.Orders
+{
+	public interface IOrderRepository
+	{
+		bool IsOrderItemsValid(List<OrderItem> orderItems);
+		Order GetOrderById(Guid id);
+
+		void Add(Order order);
+	}
+}
