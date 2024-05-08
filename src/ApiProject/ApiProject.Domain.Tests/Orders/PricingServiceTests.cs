411c6301403194abb2be857841a6a8ba573de35c
binxiong <binxiong.tea@redone.com.my>
2024-05-08 18:04:42 +0800

    Add new test case to PricingServiceTests

--- src/ApiProject/ApiProject.Domain.Tests/Orders/PricingServiceTests.cs
+++ src/ApiProject/ApiProject.Domain.Tests/Orders/PricingServiceTests.cs
@@ -55,5 +55,51 @@ namespace ApiProject.Domain.Tests.Orders
 			Assert.Equal(expectedCurrency, _sut.TotalPriceAfterTax.Currency.Code);
 		}
 
+
+		[Fact]
+		public void PricingWithTaxCalculateShouldBeValid()
+		{
+			var expectedTotalPrice = 80;
+			var expectedTotalPriceIncludeTax = 72;
+			var expectedCurrency = "SGD";
+
+			var productA = Product.Create(
+				Guid.NewGuid(),
+				"product A",
+				"product Description A",
+				Money.From(10, Currency.SGD),
+				"Cloth",
+				DateTime.UtcNow
+			);
+
+			var productB = Product.Create(
+				Guid.NewGuid(),
+				"product B",
+				"product Description B",
+				Money.From(20, Currency.SGD),
+				"Cloth",
+				DateTime.UtcNow
+			);
+
+			List<OrderItem> orderItems = new List<OrderItem>();
+
+			orderItems.Add(OrderItem.Create(productA, 2));
+
+			orderItems.Add(OrderItem.Create(productB, 3));
+
+			var pricingService = new PricingService();
+
+			var _sut = pricingService.CalculatePrice(orderItems, 0.1m);
+
+			Assert.Equal(expectedTotalPrice, _sut.TotalPrice.Amount);
+			Assert.Equal(expectedTotalPriceIncludeTax, _sut.TotalPriceAfterTax.Amount);
+
+			Assert.Equal(expectedTotalPrice, _sut.TotalPrice.Amount);
+			Assert.Equal(expectedTotalPriceIncludeTax, _sut.TotalPriceAfterTax.Amount);
+
+			Assert.Equal(expectedCurrency, _sut.TotalPrice.Currency.Code);
+			Assert.Equal(expectedCurrency, _sut.TotalPriceAfterTax.Currency.Code);
+		}
+
 	}
 }
